2c63b4b4c3e035417cdfb344b0d60b013649679a
naxrohan <rohan.jd@hotmail.com>
2022-08-20 23:22:07 +0100

    Side bar position fixed

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -25,108 +25,115 @@ const Sidebar = () => {
       sx={{ 
         display: { 
           xs: "none", 
-          sm: "flex", 
-        },
-        flexDirection: 'column', 
-        justifyContent:'space-between', 
-        height: '100vh' 
+          sm: "block", 
+        }
         }}>
+          <Box position='fixed'
+            sx={{ 
+              display: 'flex',
+              flexDirection: 'column', 
+              justifyContent:'space-between', 
+              height: '100vh' 
+             }}>
+            <List
+              sx={{
+                width: '100%',
+                maxWidth: 360,
+                bgcolor: 'background.paper',
+                color: 'text.secondary',
+                paddingLeft: '20px'
+              }}
+              component="nav">
 
-      <List
-        sx={{
-          width: '100%',
-          maxWidth: 360,
-          bgcolor: 'background.paper',
-          color: 'text.secondary',
-          paddingLeft: '20px'
-        }}
-        component="nav">
+              <ListItem disablePadding>
+                <ListItemAvatar>
+                  <UserBox>
+                    <Avatar alt="Rohan DSouza" src="https://avatars.githubusercontent.com/u/10266365?v=4" />
+                  </UserBox>
+                </ListItemAvatar>
+                <ListItemText
+                  primary="Rohan Dsouza"
+                  secondary={
+                    <Typography
+                    sx={{ display: 'inline' }}
+                      component="span"
+                      variant="body2"
+                      color="text.primary">
+                      Fullstack developer
+                    </Typography>
+                  } />
 
-        <ListItem disablePadding>
-          <ListItemAvatar>
-            <UserBox>
-              <Avatar alt="Rohan DSouza" src="https://avatars.githubusercontent.com/u/10266365?v=4" />
-            </UserBox>
-          </ListItemAvatar>
-          <ListItemText
-            primary="Rohan Dsouza"
-            secondary={
-              <Typography
-                sx={{ display: 'inline' }}
-                component="span"
-                variant="body2"
-                color="text.primary">
-                Fullstack developer
-              </Typography>
-            } />
+              </ListItem>
+              <Divider variant="inset" component="li" />
 
-        </ListItem>
-        <Divider variant="inset" component="li" />
+              <ListItem disablePadding>
+                <ListItemButton>
+                  <ListItemIcon>
+                    <Home />
+                  </ListItemIcon>
+                  <ListItemText primary="Home" />
+                </ListItemButton>
+              </ListItem>
+              <Divider variant="inset" component="li" />
 
-        <ListItem disablePadding>
-          <ListItemButton>
-            <ListItemIcon>
-              <Home />
-            </ListItemIcon>
-            <ListItemText primary="Home" />
-          </ListItemButton>
-        </ListItem>
-        <Divider variant="inset" component="li" />
+              <ListItem disablePadding>
+                <ListItemButton>
+                  <ListItemIcon>
+                    <AndroidIcon/>
+                  </ListItemIcon>
+                  <ListItemText primary="Apps" />
+                </ListItemButton>
+              </ListItem>
+              <Divider variant="inset" component="li" />
 
-        <ListItem disablePadding>
-          <ListItemButton>
-            <ListItemIcon>
-              <AndroidIcon/>
-            </ListItemIcon>
-            <ListItemText primary="Apps" />
-          </ListItemButton>
-        </ListItem>
-        <Divider variant="inset" component="li" />
+              <ListItem disablePadding>
+                <ListItemButton>
+                  <ListItemIcon>
+                    <AppsIcon />
+                  </ListItemIcon>
+                  <ListItemText primary="My Work" />
+                </ListItemButton>
+              </ListItem>
+              <Divider variant="inset" component="li" />
 
-        <ListItem disablePadding>
-          <ListItemButton>
-            <ListItemIcon>
-              <AppsIcon />
-            </ListItemIcon>
-            <ListItemText primary="My Work" />
-          </ListItemButton>
-        </ListItem>
-        <Divider variant="inset" component="li" />
+              <ListItem disablePadding>
+                <ListItemButton>
+                  <ListItemIcon>
+                    <RssFeedIcon />
+                  </ListItemIcon>
+                  <ListItemText primary="Blog" />
+                </ListItemButton>
+              </ListItem>
+              <Divider variant="inset" component="li" />
 
-        <ListItem disablePadding>
-          <ListItemButton>
-            <ListItemIcon>
-              <RssFeedIcon />
-            </ListItemIcon>
-            <ListItemText primary="Blog" />
-          </ListItemButton>
-        </ListItem>
-        <Divider variant="inset" component="li" />
+              <ListItem disablePadding>
+                <ListItemButton>
+                  <ListItemIcon>
+                    <InfoIcon />
+                  </ListItemIcon>
+                  <ListItemText primary="About" />
+                </ListItemButton>
+              </ListItem>
 
-        <ListItem disablePadding>
-          <ListItemButton>
-            <ListItemIcon>
-              <InfoIcon />
-            </ListItemIcon>
-            <ListItemText primary="About" />
-          </ListItemButton>
-        </ListItem>
+            </List>
 
-      </List>
-
-      <Stack direction='row' justifyContent='center'>
-        <ButtonGroup variant="text" aria-label="text button group">
-          <Button>
-            <LinkedInIcon />
-          </Button>
-          <Button>
-            <GitHubIcon />
-          </Button>
-          <Button>
-            <AlternateEmailIcon/>
-          </Button>
-        </ButtonGroup>
-      </Stack>
+            <Stack 
+              direction='row' 
+              justifyContent='center'
+              sx={{ marginBottom: '10px' }}>
+              <ButtonGroup variant="text" aria-label="text button group">
+                <Button>
+                  <LinkedInIcon />
+                </Button>
+                <Button>
+                  <GitHubIcon />
+                </Button>
+                <Button>
+                  <AlternateEmailIcon/>
+                </Button>
+              </ButtonGroup>
+            </Stack>
+        </Box>
     </Box>
   )
 }
