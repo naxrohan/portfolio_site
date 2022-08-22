662b3357872c861d18cfd6b7caf222685253fe14
naxrohan <rohan.jd@hotmail.com>
2022-08-22 19:21:09 +0100

    ResponsiveAppBar added to Home page

---
+++ src/components/ResponsiveAppBar.jsx
@@ -0,0 +1,111 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
+import PagesData from '../JsonData/PagesData'
+import { Link } from 'react-router-dom';
+
+const pages = ['Products', 'Pricing', 'Blog'];
+
+
+const ResponsiveAppBar = () => {
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  return (
+    <AppBar position="static" sx={{
+        display: {
+          xs: "block",
+          sm: "block",
+          md: "none",
+        },
+      }}>
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          
+          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'left',
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: 'block', md: 'none' },
+              }}
+            >
+              {PagesData.map((page) => (
+                <MenuItem key={page} onClick={handleCloseNavMenu}>
+                    <Link to={page.page} style={{  textDecoration: 'none' }}>
+                        <Typography textAlign="center">{page.title}</Typography>
+                    </Link>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+          <Typography
+            variant="h5"
+            noWrap
+            component="a"
+            href=""
+            sx={{
+              mr: 2,
+              display: { xs: 'flex', md: 'none' },
+              flexGrow: 1,
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            LOGO-a
+          </Typography>
+
+          <Box sx={{ flexGrow: 0 }}>
+            <Tooltip title="Open settings">
+              <IconButton sx={{ p: 0 }}>
+                <Avatar alt="Rohan DSouza" src="https://avatars.githubusercontent.com/u/10266365?v=4" />
+              </IconButton>
+            </Tooltip>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+};
+export default ResponsiveAppBar;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,6 +3,7 @@ import Box from '@mui/material/Box';
 import Stack from '@mui/material/Stack';
 import Sidebar from '../components/Sidebar';
 import HomeContent from '../components/HomeContent';
+import ResponsiveAppBar from '../components/ResponsiveAppBar';
 
 
 const HomePage = () => {
@@ -10,6 +11,7 @@ const HomePage = () => {
     <Box 
       bgcolor={"background.default"} 
       color={"text.primary"}>
+        <ResponsiveAppBar />
       <Stack direction='row' spacing={1} justifyContent='space-between'>
         <Sidebar />
         <HomeContent />
