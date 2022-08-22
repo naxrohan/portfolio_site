7876109a51a0acb026192f1a49beab86b26beb13
naxrohan <rohan.jd@hotmail.com>
2022-08-22 13:02:21 +0100

    Add router & links to sidebar

--- src/App.js
+++ src/App.js
@@ -2,13 +2,27 @@ import MyApps from "./pages/MyApps";
 import HomePage from "./pages/HomePage";
 import MyWork from "./pages/MyWork";
 import Blog from "./pages/Blog";
+import AppsPage from "./pages/AppsPage"
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate
+} from "react-router-dom"
 
 function App() {
   return (
-    <HomePage />
-    // <MyApps />
-    // <MyWork />
-    // <Blog />
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        
+        <Route path="/apps" element={<MyApps />} />
+        <Route path="/apps/:appName" element={<AppsPage />} />
+        
+        <Route path="/my-work" element={<MyWork />} />
+        <Route path="/blog" element={<Blog />} />
+      </Routes>
+    </Router>
   );
 }
 
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -10,6 +10,7 @@ import GitHubIcon from '@mui/icons-material/GitHub';
 import AlternateEmailIcon from '@mui/icons-material/AlternateEmail';
 import AndroidIcon from '@mui/icons-material/Android';
 import RssFeedIcon from '@mui/icons-material/RssFeed';
+import { Link } from 'react-router-dom';
 
 const UserBox = styled("div")(({ theme }) => ({
   display: 'flex',
@@ -22,118 +23,127 @@ const Sidebar = () => {
   return (
     <Box
       flex={1}
-      sx={{ 
-        display: { 
-          xs: "none", 
-          sm: "block", 
+      sx={{
+        display: {
+          xs: "none",
+          sm: "block",
         }
+      }}>
+      <Box position='fixed'
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'space-between',
+          height: '100vh'
         }}>
-          <Box position='fixed'
-            sx={{ 
-              display: 'flex',
-              flexDirection: 'column', 
-              justifyContent:'space-between', 
-              height: '100vh' 
-             }}>
-            <List
-              sx={{
-                width: '100%',
-                maxWidth: 360,
-                bgcolor: 'background.paper',
-                color: 'text.secondary',
-                paddingLeft: '20px'
-              }}
-              component="nav">
+        <List
+          sx={{
+            width: '100%',
+            maxWidth: 360,
+            bgcolor: 'background.paper',
+            color: 'text.secondary',
+            paddingLeft: '20px'
+          }}
+          component="nav">
 
-              <ListItem disablePadding>
-                <ListItemAvatar>
-                  <UserBox>
-                    <Avatar alt="Rohan DSouza" src="https://avatars.githubusercontent.com/u/10266365?v=4" />
-                  </UserBox>
-                </ListItemAvatar>
-                <ListItemText
-                  primary="Rohan Dsouza"
-                  secondary={
-                    <Typography
-                    sx={{ display: 'inline' }}
-                      component="span"
-                      variant="body2"
-                      color="text.primary">
-                      Fullstack developer
-                    </Typography>
-                  } />
+          <ListItem disablePadding>
+            <ListItemAvatar>
+              <UserBox>
+                <Avatar alt="Rohan DSouza" src="https://avatars.githubusercontent.com/u/10266365?v=4" />
+              </UserBox>
+            </ListItemAvatar>
+            <ListItemText
+              primary="Rohan Dsouza"
+              secondary={
+                <Typography
+                  sx={{ display: 'inline' }}
+                  component="span"
+                  variant="body2"
+                  color="text.primary">
+                  Fullstack developer
+                </Typography>
+              } />
 
-              </ListItem>
-              <Divider variant="inset" component="li" />
+          </ListItem>
+          <Divider variant="inset" component="li" />
 
-              <ListItem disablePadding>
-                <ListItemButton>
-                  <ListItemIcon>
-                    <Home />
-                  </ListItemIcon>
-                  <ListItemText primary="Home" />
-                </ListItemButton>
-              </ListItem>
-              <Divider variant="inset" component="li" />
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <Home />
+              </ListItemIcon>
+              <Link to={'/'} style={{  textDecoration: 'none' }}>
+                <ListItemText primary="Home" />
+              </Link>
+            </ListItemButton>
+          </ListItem>
+          <Divider variant="inset" component="li" />
 
-              <ListItem disablePadding>
-                <ListItemButton>
-                  <ListItemIcon>
-                    <AndroidIcon/>
-                  </ListItemIcon>
-                  <ListItemText primary="Apps" />
-                </ListItemButton>
-              </ListItem>
-              <Divider variant="inset" component="li" />
+          <ListItem disablePadding>
+            <ListItemButton>
 
-              <ListItem disablePadding>
-                <ListItemButton>
-                  <ListItemIcon>
-                    <AppsIcon />
-                  </ListItemIcon>
-                  <ListItemText primary="My Work" />
-                </ListItemButton>
-              </ListItem>
-              <Divider variant="inset" component="li" />
+              <ListItemIcon>
+                <AndroidIcon />
+              </ListItemIcon>
+              <Link to={'/apps'} style={{  textDecoration: 'none' }}>
+                <ListItemText primary="Apps" />
+              </Link>
+            </ListItemButton>
+          </ListItem>
+          <Divider variant="inset" component="li" />
 
-              <ListItem disablePadding>
-                <ListItemButton>
-                  <ListItemIcon>
-                    <RssFeedIcon />
-                  </ListItemIcon>
-                  <ListItemText primary="Blog" />
-                </ListItemButton>
-              </ListItem>
-              <Divider variant="inset" component="li" />
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <AppsIcon />
+              </ListItemIcon>
+              <Link to={'/my-work'} style={{  textDecoration: 'none' }}>
+                <ListItemText primary="My Work" />
+              </Link>
+            </ListItemButton>
+          </ListItem>
+          <Divider variant="inset" component="li" />
 
-              <ListItem disablePadding>
-                <ListItemButton>
-                  <ListItemIcon>
-                    <InfoIcon />
-                  </ListItemIcon>
-                  <ListItemText primary="About" />
-                </ListItemButton>
-              </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <RssFeedIcon />
+              </ListItemIcon>
+              <Link to={'/blog'} style={{  textDecoration: 'none' }}>
+                <ListItemText primary="Blog" />
+              </Link>
+            </ListItemButton>
+          </ListItem>
+          <Divider variant="inset" component="li" />
 
-            </List>
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <InfoIcon />
+              </ListItemIcon>
+              <ListItemText primary="About" />
+            </ListItemButton>
+          </ListItem>
 
-            <Stack 
-              direction='row' 
-              justifyContent='center'
-              sx={{ marginBottom: '10px' }}>
-              <ButtonGroup variant="text" aria-label="text button group">
-                <Button>
-                  <LinkedInIcon />
-                </Button>
-                <Button>
-                  <GitHubIcon />
-                </Button>
-                <Button>
-                  <AlternateEmailIcon/>
-                </Button>
-              </ButtonGroup>
-            </Stack>
-        </Box>
+        </List>
+
+        <Stack
+          direction='row'
+          justifyContent='center'
+          sx={{ marginBottom: '10px' }}>
+          <ButtonGroup variant="text" aria-label="text button group">
+            <Button>
+              <LinkedInIcon />
+            </Button>
+            <Button>
+              <GitHubIcon />
+            </Button>
+            <Button>
+              <AlternateEmailIcon />
+            </Button>
+          </ButtonGroup>
+        </Stack>
+      </Box>
     </Box>
   )
 }
