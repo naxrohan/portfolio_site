47a32c2cf5284baed56bc8615c363d27aa8d15f5
naxrohan <rohan.jd@hotmail.com>
2022-08-23 22:20:32 +0100

    Show data from Json file

--- src/components/BlogListing.jsx
+++ src/components/BlogListing.jsx
@@ -1,12 +1,23 @@
 import { Stack, styled } from '@mui/material';
 import { Box } from '@mui/system'
 import BlogArticleItem from './BlogArticleItem';
+import BlogData from "../JsonData/BlogData"
+import { useEffect, useState } from 'react';
 
 const MainBody = styled(Stack)(() => ({
     marginTop: '50px'
 }));
 
 const BlogListing = () => { 
+    
+    const [BlogItems, setBlogItems] = useState(BlogData.rss.channel.item);
+    const [BlogCategories, setBlogCategories] = useState(BlogData.rss.channel.tags);
+
+    useEffect(()=>{
+        setBlogItems(BlogData.rss.channel.item);
+        setBlogCategories(BlogData.rss.channel.tags);
+    },[]);
+
     return (
         <Box
             bgcolor={"background.default"}
@@ -15,10 +26,14 @@ const BlogListing = () => {
             sx={{ height: '100vh', overflowY: 'scroll', paddingLeft: { xs : 5, md: 10}, paddingRight: { xs : 5, md: 10} }}>
         
             <MainBody spacing={2} justifyContent='space-evenly'>
-                <BlogArticleItem type="short" />
-                <BlogArticleItem type="short"/>
-                <BlogArticleItem type="short"/>
-                <BlogArticleItem type="short"/>
+                {/* <BlogArticleItem type="short" /> */}
+                {BlogItems.map((item, key) => (
+                    //item !== undefined &&
+                        <BlogArticleItem 
+                        key={key}
+                        type="short" 
+                        jsonData={item} />
+                ))}
             </MainBody>
         
         </Box>
--- src/pages/BlogsPage.jsx
+++ src/pages/BlogsPage.jsx
@@ -3,9 +3,20 @@ import Sidebar from '../components/Sidebar'
 import { Box, createTheme, Stack, styled, ThemeProvider } from '@mui/material'
 import ResponsiveAppBar from '../components/ResponsiveAppBar';
 import BlogArticleItem from '../components/BlogArticleItem';
+import { useParams } from 'react-router-dom';
+import BlogData from "../JsonData/BlogData"
+
 
 const BlogsPage = () => {
+
+  const param = useParams();
   const [mode, setMode] = useState("dark");
+  const [BlogItems, setBlogItems] = useState(BlogData.rss.channel.item);
+  const [BlogCategories, setBlogCategories] = useState(BlogData.rss.channel.tags);
+  
+  const BlogPostDetails = BlogItems.filter(item => {
+      return item.wp_post_name === param.articleName;
+  });
   const darkTheme = createTheme({
     palette: {
       mode: mode
@@ -30,7 +41,9 @@ const BlogsPage = () => {
             flex={5}
             sx={{ height: '100vh', overflowY: 'scroll', paddingLeft: { xs : 5, md: 10}, paddingRight: { xs : 5, md: 10} }}>
               <MainBody spacing={2} justifyContent='space-evenly'>
-                <BlogArticleItem type="full" />
+                <BlogArticleItem 
+                  type="full" 
+                  jsonData={BlogPostDetails[0]} />
               </MainBody>
         </Box>
         </Stack>
